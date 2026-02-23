9ea0ebf24dc5f70917320158e6264fa1e2447915
Sanjay Kumar <sanjaykumarmogali16@gmail.com>
2026-02-23 19:59:45 +0530

    2nd time SDF testing

--- src/FileCabinet/SuiteScripts/Amazon pay Refund.js
+++ src/FileCabinet/SuiteScripts/Amazon pay Refund.js
@@ -8,7 +8,7 @@ define(['N/record', 'N/https', 'N/url', 'N/search'],
 
         const beforeLoad = (scriptContext) => {
             var refundRecord = scriptContext.newRecord;
-            log.debug('refundRecord from SDF deployment', refundRecord)
+            log.debug('refundRecord from SDF deployment testing process', refundRecord)
             var paymentMethod = refundRecord.getValue({ fieldId: 'paymentmethod' });
             log.debug('paymentMethod', paymentMethod)
 
